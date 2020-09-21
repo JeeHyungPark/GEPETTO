4c80307d482041db5735d8737b31673a5ab0efb4
JeeHyungPark <code.paori@gmail.com>
2020-09-21 16:15:37 +0900

    [#1] Implement extract 'together' in dataset

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ statement_list= []
 who_list= []
 when_list= []
 where_list= []
-with_list= []
+together_list= []
 what_list= []
 
 for datafile in file_list: 
@@ -18,17 +18,26 @@ for datafile in file_list:
     who= unit[0]  
     who_list.append(who) 
     
-    for where in unit:
-        if "에서" in where:
-            where_list.append(where)
-            index= unit.index(where)
+    for word in unit:
+        if "에서" in word:
+            where_list.append(word)
+            index= unit.index(word)
             when= unit[1:index]
             when= ' '.join(when)
             when_list.append(when)
-
+        if "와" in word:
+            together_list.append(word)
+        elif "과" in word:
+            together_list.append(word)
+        elif "랑" in word:
+            together_list.append(word)
+        elif "혼자" in word:
+            together_list.append(word)
+    
 print(who_list)
 print(where_list)
 print(when_list)
+print(together_list)
 '''
 datalist로 이차원 리스트 생성할 것임
     누가    언제    어디서  누구랑  무엇을
