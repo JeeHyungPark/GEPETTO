5713c18c5a23a70a233f1cc14ff3bcaf77bc6439
JeeHyungPark <code.paori@gmail.com>
2020-09-21 16:23:12 +0900

    [#1] Implement extract 'what' in dataset

--- data.py
+++ data.py
@@ -21,23 +21,40 @@ for datafile in file_list:
     for word in unit:
         if "에서" in word:
             where_list.append(word)
-            index= unit.index(word)
-            when= unit[1:index]
+            where_index= unit.index(word)
+            when= unit[1:where_index]
             when= ' '.join(when)
             when_list.append(when)
         if "와" in word:
             together_list.append(word)
+            together_index= unit.index(word)
+            what= unit[together_index:]
+            what= ' '.join(what)
+            what_list.append(what)
         elif "과" in word:
             together_list.append(word)
+            together_index= unit.index(word)
+            what= unit[together_index:]
+            what= ' '.join(what)
+            what_list.append(what)
         elif "랑" in word:
             together_list.append(word)
+            together_index= unit.index(word)
+            what= unit[together_index:]
+            what= ' '.join(what)
+            what_list.append(what)
         elif "혼자" in word:
             together_list.append(word)
+            together_index= unit.index(word)
+            what= unit[together_index:]
+            what= ' '.join(what)
+            what_list.append(what)
     
 print(who_list)
 print(where_list)
 print(when_list)
 print(together_list)
+print(what_list)
 '''
 datalist로 이차원 리스트 생성할 것임
     누가    언제    어디서  누구랑  무엇을
