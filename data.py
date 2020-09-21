72e3df55daa221942dbf69b9e545e4e0abfb26e0
JeeHyungPark <code.paori@gmail.com>
2020-09-21 14:56:43 +0900

    [#1] Implement extract 'where' in dataset

--- data.py
+++ data.py
@@ -4,21 +4,27 @@ import os
 
 path_dir= './dataset/'
 file_list= os.listdir(path_dir) 
-statement_list= [] 
+statement_list= []
+who_list= []
+when_list= []
+where_list= []
+with_list= []
+what_list= []
 
 for datafile in file_list: 
     f= open(path_dir+datafile, 'r')  
     statement= f.readline()
     unit= statement.split() 
     who= unit[0]  
-    statement_list.append(who) 
+    who_list.append(who) 
     
-    # for "에서" in unit:
-    #     if where in unit:
-    #         statement_list[2].append(word)
-    #         print(word)
+    for where in unit:
+        if "에서" in where:
+            where_list.append(where)
     
-print(statement_list)
+print(who_list)
+print(where_list)
+
 '''
 datalist로 이차원 리스트 생성할 것임
     누가    언제    어디서  누구랑  무엇을
