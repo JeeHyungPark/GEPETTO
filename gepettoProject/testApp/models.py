2e030115ab9e57b707347aaf2b88ff0791282349
JeeHyungPark <code.paori@gmail.com>
2020-11-02 21:41:43 +0900

    [#14] Create Test model

--- gepettoProject/testApp/models.py
+++ gepettoProject/testApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    ANSWER = [
+        (O, 'O'),
+        (X, 'X'),
+    ]
+    tester = models.ForeignKey(get_user_model(), on_delete=models.set_NULL, null=True, blank=True, relate_name='tests')
+    text = models.TextField()
+    question1 = models.TextField()
+    question2 = models.TextField()
+    question3 = models.TextField()
+    answer1 = models.CharField(
+        max_length = 1,
+        choices = ANSWER,
+        default = O,
+    )
+    answer2 = models.CharField(
+        max_length = 1,
+        choices = ANSWER,
+        default = O,
+    )
+    answer3 = models.CharField(
+        max_length = 1,
+        choices = ANSWER,
+        default = O,
+    )
+    test_probability = models.CharField(
+        max_length = 80,
+        default = 0,
+    )
