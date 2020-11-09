df93e83b6b43b6881592b6409c532d6469de78f7
JeeHyungPark <code.paori@gmail.com>
2020-11-09 23:59:46 +0900

    to Merge

--- gepettoProject/mainApp/urls.py
+++ gepettoProject/mainApp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', mainApp.views.signup, name='signup'),
     path('mypage/', mainApp.views.mypage, name='mypage'),
     path('mypage-edit/', mainApp.views.mypage_edit,name='mypage_edit'),
+    path('logout/', mainApp.views.logout, name='logout'),
 ]
--- gepettoProject/mainApp/views.py
+++ gepettoProject/mainApp/views.py
@@ -1,20 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import *
+from django.contrib import auth
+from django.contrib.auth import authenticate, login, logout
 import os, sys
 import random
 import requests
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            if 'nickname' in request.POST:
+                nickname = request.POST['nickname']
+                password = request.POST['password']
+                user = authenticate(request, nickname=nickname, password=password)
+                if user is not None:
+                    auth.login(request, user)
+                    return redirect('login')
+                else:
+                    return render(request, 'testApp/input.html')
+        return render(request, 'login.html')
+    else:
+        return render(request, 'testApp/input.html')
 
 def main(request):
     return render(request, 'main.html')
 
 def signup(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST['email'], request.POST['nickname'], request.POST['password'])
+        user.gender = request.POST['gender']
+        user.age = request.POST['age']
+        user.save
+        return render(request, 'login.html')
     return render(request, 'signup.html')
 
 def mypage(request):
-    return render(request, 'mypage.html')
+    user = request.user
+    username = request.user.nickname
+    return render(request, 'mypage.html', {'user':user, 'username':username})
 
 def mypage_edit(request):
     return render(request, 'mypage_edit.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('login')
