182e8b7e72eb9050fc779e74b1f84c990bf56c4a
YuRi2Song <dbfldi10@likelion.org>
2020-11-09 23:57:31 +0900

    mainApp

--- gepettoProject/mainApp/views.py
+++ gepettoProject/mainApp/views.py
@@ -1,20 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os, sys
 import random
 import requests
+from django.views.generic.edit import FormView
+from . import forms
+from django.contrib.auth import login,authenticate, logout
+from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from .models import User
+from django.db.models import Q
 
-
-def login(request):
-    return render(request, 'login.html')
 
 def main(request):
     return render(request, 'main.html')
 
-def signup(request):
-    return render(request, 'signup.html')
+class SignupView(FormView):
+    """sign up user view"""
+    form_class = forms.SignupForm
+    template_name = 'signup.html'
+    success_url = 'http://127.0.0.1:8000/test'
+
+    def form_valid(self, form):
+        """ process user signup"""
+        user = form.save(commit=False)
+        user.save()
+        
+        print(user.nickname)
+        print(user.email)
+        print(user.password)
+        login(self.request, user)
+        if user is not None:
+            return HttpResponseRedirect(self.success_url)
+
+        return super().form_valid(form)
+
+
+class LoginView(FormView):
+    """login view"""
+
+    form_class = forms.LoginForm
+    success_url = 'http://127.0.0.1:8000/test'
+    template_name = 'login.html'
+
+    def form_valid(self, form):
+        """ process user login"""
+        credentials = form.cleaned_data
+        queryset = User.objects.all()
+        user = queryset.filter(email=credentials['email'], password=credentials['password'])
+        print(queryset)
+        print(user[0])
+
+        if user is not None:
+            login(self.request, user[0])
+            return HttpResponseRedirect(self.success_url)
+
+        else:
+            messages.add_message(self.request, messages.INFO, 'Wrong credentials\
+                                please try again')
+            return HttpResponseRedirect('http://127.0.0.1:8000/login')
+
+
+def Logout(request):
+    logout(request)
+    return HttpResponseRedirect('http://127.0.0.1:8000/')
 
 def mypage(request):
-    return render(request, 'mypage.html')
+    cur_user = request.user
+
+    if cur_user.is_authenticated:  
+        return render(request, 'mypage.html')
+    else:
+        return redirect('main')
 
 def mypage_edit(request):
     return render(request, 'mypage_edit.html')
